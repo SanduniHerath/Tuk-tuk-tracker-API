9c6fbda39300290cbd7828e4059118ad09b1ccf2
Sanduni <sandunisathsarani09@gmail.com>
2026-04-08 23:29:21 +0530

    initial project setup, express server running

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+// Simple route
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
